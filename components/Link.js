7c19e280fdec4fc1c6bf0eb52886b4d4da15c9b0
djakish <69299717+djakish@users.noreply.github.com>
2022-05-20 20:54:00 +0600

    ssr

--- components/Link.js
+++ components/Link.js
@@ -4,7 +4,7 @@ export default function Link({title,url}) {
     const link = url.includes("http") ? url : `https://${url}`;
     return (
       <NextLink href={link}>
-        <button className="bg-emerald-400 text-white border-green-300 border-2 py-4 px-14 w-full hover:bg-white hover:text-green-300 transition-all duration-100">
+        <button className="bg-emerald-400 text-white border-emerald-300 border-2 py-4 px-14 w-full  hover:bg-white hover:text-emerald-300 transition-all duration-100">
           {title}
         </button>
       </NextLink>
--- pages/[username].js
+++ pages/[username].js
@@ -4,79 +4,7 @@ import { supabase } from '../utils/supabaseClient'
 import  Link from '../components/Link'
 import Image from 'next/image'
 
-const LinkPage = ({profile}) => {
-
-
-  const [links, setLinks] = useState([]);
-  const [user_id, setUserId] = useState(null);
-  const [website, setWebsite] = useState(null);
-  const [avatar_url, setAvatarUrl] = useState(null);
-
-  useEffect(() => {
-    getLinks()
-    if (profile.avatar_url) downloadImage(profile.avatar_url)
-}, [])
-
-  async function getProfile() {
-    try {
-    const { username } = await router.query
-
-      let { data, error, status } = await supabase
-        .from('profiles')
-        .select('*')
-        .eq('username', username)
-        .single()
-
-      if (error && status !== 406) {
-        throw error
-      }
-
-      if (data) {
-          console.log(data.id)
-        setUserId(data.id)
-
-        if (data.avatar_url) downloadImage(data.avatar_url)
-
-      }
-    } catch (error) {
-      alert(error.message)
-    } finally {
-    }
-  }
-
-  async function getLinks() {
-    try {
-
-      let { data, error, status } = await supabase
-        .from('links')
-        .select(`*`)
-        .eq('user_id', profile.id)
-
-      if (error && status !== 406) {
-        throw error
-      }
-
-      if (data) {
-        setLinks(data)
-      }
-    } catch (error) {
-      alert(error.message)
-    } finally {
-    }
-  }
-
-  async function downloadImage(path) {
-    try {
-      const { data, error } = await supabase.storage.from('avatars').download(path)
-      if (error) {
-        throw error
-      }
-      const url = URL.createObjectURL(data)
-      setAvatarUrl(url)
-    } catch (error) {
-      console.log('Error downloading image: ', error.message)
-    }
-  }
+const LinkPage = ({profile,avatar_url,links}) => {
 
 
   return (
@@ -116,16 +44,52 @@ export const getStaticPaths = async () => {
     };
   };
 
+
+  function downloadImage(path) {
+    return "https://elhwmvlgafwmydwmaumb.supabase.co/storage/v1/object/public/avatars/" + path
+  }
+  
 export const getStaticProps = async ({ params: { username } }) => {
     let { data: profile, error, status } = await supabase
         .from('profiles')
         .select('*')
         .eq('username', username)
         .single()
+
+
+    let avatar_url = null;
+
+    if(profile.avatar_url != null){
+      avatar_url = downloadImage(profile.avatar_url)
+    }
+    
+
+
+      let links = [];
+        try {
+
+          let { data, error, status } = await supabase
+            .from('links')
+            .select(`*`)
+            .eq('user_id', profile.id)
+    
+          if (error && status !== 406) {
+            throw error
+          }
+    
+          if (data) {
+            links = data
+          }
+        } catch (error) {
+          alert(error.message)
+        } finally {
+        }
   
     return {
       props: {
         profile,
+        avatar_url,
+        links
       },
     };
   };
