af4b97ad564792d03b33293162a9599079c03c42
djakish <69299717+djakish@users.noreply.github.com>
2022-06-20 03:34:06 +0600

    lol triangle

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer({ title, url }) {
   return (
 
     <footer className="h-10 flex flex-row text-white gap-2 fixed bottom-5">
-      Made Using Next.js and Supabase 💚 by 
+      Made Using Next.js ▲ and Supabase 💚 by 
       <NextLink href="https://github.com/djakish">
          me :).
       </NextLink>
